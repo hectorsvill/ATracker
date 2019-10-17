736674a1d7cca5394a2e5ba12b0da3062301460e
hectorsvill <hectorstevenvillasno@gmail.com>
2019-10-16 22:56:13 -0700

    implemented NSTableView in ATrackerViewController

---
+++ ATracker/ATrackerViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  ATracker
+//
+//  Created by Hector on 10/16/19.
+//  Copyright © 2019 Hector. All rights reserved.
+//
+
+import Cocoa
+
+class ATrackerViewController: NSViewController {
+    @IBOutlet weak var tableView: NSTableView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        }
+    }
+}
+
+extension ATrackerViewController: NSTableViewDataSource{
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return 5
+    }
+}
+
+extension ATrackerViewController: NSTableViewDelegate {
+    
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        
+        let cell = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as! NSTableCellView
+        
+        cell.textField?.stringValue = "\(row)"
+        
+        return cell
+    }
+
+}
