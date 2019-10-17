e11fe38f159e7da3fb3ed5ce09d122049d02b05f
hectorsvill <hectorstevenvillasno@gmail.com>
2019-10-16 20:10:23 -0700

    Initial Commit

---
+++ ATracker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ATracker
+//
+//  Created by Hector on 10/16/19.
+//  Copyright © 2019 Hector. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ATracker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ATracker
+//
+//  Created by Hector on 10/16/19.
+//  Copyright © 2019 Hector. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
