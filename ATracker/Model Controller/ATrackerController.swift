a304362d68812a12c01717f9cbd0498062c766d6
hectorsvill <hectorstevenvillasano@gmail.com>
2019-10-20 20:45:24 -0700

    ATrack convenience init

---
+++ ATracker/Model Controller/ATrackerController.swift
@@ -0,0 +1,23 @@
+//
+//  ATrackerController.swift
+//  ATracker
+//
+//  Created by Hector on 10/20/19.
+//  Copyright © 2019 Hector. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class ATrackerController {
+    
+    var ATrackerList: [ATrack] {
+        
+        
+        return []
+    }
+    
+    
+    
+    
+}
---
+++ ATracker/Models/ATrack+Convenience.swift
@@ -0,0 +1,21 @@
+//
+//  ATrack+Convenience.swift
+//  ATracker
+//
+//  Created by Hector on 10/20/19.
+//  Copyright © 2019 Hector. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension ATrack {
+    convenience init(title: String, summary: String? = nil, start: Date, end: Date, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(context: context)
+        self.title = title
+        self.summary = summary
+        self.start = start
+        self.end = end
+    }
+    
+}
