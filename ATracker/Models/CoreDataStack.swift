2d4fc838f192d4485209887bf5bdf49f81691c4a
hectorsvill <hectorstevenvillasano@gmail.com>
2019-10-20 20:08:04 -0700

    core data stack start

---
+++ ATracker/Models/CoreDataStack.swift
@@ -0,0 +1,14 @@
+//
+//  CoreDataStack.swift
+//  ATracker
+//
+//  Created by Hector on 10/20/19.
+//  Copyright © 2019 Hector. All rights reserved.
+//
+
+import Foundation
+
+
+class CoreDataStack {
+    
+}
