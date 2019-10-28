6706f68a520ea0c25ef85a7e90e03fe1ef6ed546
hectorsvill <hectorstevenvillasano@gmail.com>
2019-10-28 10:02:39 -0700

    refactor insertEvent

--- ATracker/Model Controller/EventKitController.swift
+++ ATracker/Model Controller/EventKitController.swift
@@ -35,40 +35,28 @@ class EventKitController {
         }
     }
     
+    func fetchCalendar(with title: String) -> EKCalendar {
+        eventStore.calendars(for: .event).filter{ $0.title == calendarTitle }.first!
+    }
+    
     func insertEvent(with atrack: ATrack) {
-     
-        let calendars = eventStore.calendars(for: .event)
-        
-//        print(calendars)
-        
-        for calendar in calendars {
-            print(calendar.title)
-            
-            if calendar.title == calendarTitle {
-                print("here")
-                let startDate = atrack.start
-                let endDate = atrack.end
-                
-                let event = EKEvent(eventStore: eventStore)
-                event.calendar = calendar
-                event.title = atrack.title
-                event.notes = atrack.summary
-                
-                event.url = nil
+        let calendar = fetchCalendar(with: calendarTitle)
+        print(calendar)
 
+        let event = EKEvent(eventStore: eventStore)
+        event.calendar = calendar
+        event.title = atrack.title
+        event.notes = atrack.summary
+        event.url = nil
+        event.startDate = atrack.start
+        event.endDate = atrack.end
                 
-                event.startDate = startDate
-                event.endDate = endDate
-                
-                do {
-                    try eventStore.save(event, span: .thisEvent)
-                } catch {
-                    NSLog("Error with event: \(error)")
-                }
-                
-                return
-            }
+        do {
+            try eventStore.save(event, span: .thisEvent)
+        } catch {
+            NSLog("Error with event: \(error)")
         }
+                
     }
     
     
