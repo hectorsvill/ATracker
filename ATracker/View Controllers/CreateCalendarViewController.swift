3b8411f43760e1733f3bb97e13c19d4163d72530
hectorsvill <hectorstevenvillasano@gmail.com>
2019-11-03 09:51:14 -0800

    CreateCalendarViewController

---
+++ ATracker/View Controllers/CreateCalendarViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CreateCalendarViewController.swift
+//  ATracker
+//
+//  Created by Hector on 11/3/19.
+//  Copyright © 2019 Hector. All rights reserved.
+//
+
+import Cocoa
+
+class CreateCalendarViewController: NSViewController {
+
+    @IBOutlet var calendarTitleTextField: NSTextField!
+    @IBOutlet var segmentControl: NSSegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBAction func createCalendarButtonPressed(_ sender: Any) {
+    }
+}
