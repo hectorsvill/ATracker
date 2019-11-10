f8e73d87589e7e0cdafced2b35c56fa33d71426e
hectorsvill <hectorstevenvillasano@gmail.com>
2019-11-10 15:03:01 -0800

    unit test

--- ATrackerUnitTests/ATrackerUnitTests.swift
+++ ATrackerUnitTests/ATrackerUnitTests.swift
@@ -7,25 +7,19 @@
 //
 
 import XCTest
+@testable import ATracker
+
 
 class ATrackerUnitTests: XCTestCase {
 
-   @testable import ATracker
-
-
-   class ATrackerUnitTests: XCTestCase {
-
-       let eventKitController = EventKitController()
-       
-       func testCreateCalendar() {
-           let calendarTitle = "Test this Calendar"
-           eventKitController.createNewCalendar(with: calendarTitle, using: .local)
-           
-           let calendarTitles = eventKitController.eventCalendars.map { $0.title }
-           XCTAssertTrue(calendarTitles.contains(calendarTitle))
-       }
-
-   }
-
+    let eventKitController = EventKitController()
+    
+    func testCreateCalendar() {
+        let calendarTitle = "Test this Calendar"
+        eventKitController.createNewCalendar(with: calendarTitle, using: .local)
+        
+        let calendarTitles = eventKitController.eventCalendars.map { $0.title }
+        XCTAssertTrue(calendarTitles.contains(calendarTitle))
+    }
 
 }
